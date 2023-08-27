b40cb7e56611f96a9b5a110a0c5cd50f1ff207e6
Katty Mellado <katherine.mellado@gmail.com>
2023-08-27 15:38:54 -0400

    MOD contact php

---
+++ php/sendMail.php
@@ -0,0 +1,14 @@
+<?php 
+	$nombre = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+	$asunto = 'Contacto Fuego y Carnes'.$subject;
+	$mensaje = "Nombre: ".$nombre."<br> Email: $email<br> Mensaje:".$_POST['message'];
+
+
+	if(mail('fuegoycarnesadomicilio@gmail.com', $asunto, $mensaje)){
+		echo "Correo enviado";
+	}
+ ?>
