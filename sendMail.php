2a99439e141183ea7504c4b7691a9710f78108ee
Katty Mellado <katherine.mellado@gmail.com>
2023-11-12 00:37:17 -0300

    sin correo

--- sendMail.php
+++ sendMail.php
@@ -1,10 +1,12 @@
 <?php 
 	$nombre = $_POST['name'];
     $email = $_POST['email'];
-    $subject = $_POST['subject'];
+    //$subject = $_POST['subject'];
     $message = $_POST['message'];
 
-	$asunto = 'Contacto Fuego y Carnes'.$subject;
+	//$asunto = 'Contacto Fuego y Carnes'.$subject;
+
+	$asunto = 'Contacto Fuego y Carnes'.$name;
 	$mensaje = "Nombre: ".$nombre."<br> Email: $email<br> Mensaje:".$_POST['message'];
 
 
